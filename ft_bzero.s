1d5c6d07cb9887b7465b8f8a6789e761f407e325
Martin LE-ROY <mle-roy@e2r3p17.42.fr>
2015-02-06 16:12:46 +0100

    bzero ok pour test

--- ft_bzero.s
+++ ft_bzero.s
@@ -2,26 +2,26 @@
 
 global _ft_bzero
 
-;; _ft_bzero:
-;; 	push rbx
-;; 	mov rbx, rdi
-
-;; while:
-;; 	cmp rsi, 0
-;; 	je return
-;; 	mov byte [rbx], 0
-;; 	add rbx, 1
-;; 	add rsi, -1
-;; 	jmp while
-
-;; return:
-;; 	pop rbx
-;; 	ret
-
 _ft_bzero:
-	;; push rcx
-	mov eax, 0
-	mov rcx, rsi
-	rep stosd
-	;; pop rcx
+	push rbx
+	mov rbx, rdi
+
+while:
+	cmp rsi, 0
+	je return
+	mov byte [rbx], 0
+	add rbx, 1
+	add rsi, -1
+	jmp while
+
+return:
+	pop rbx
 	ret
+
+;; _ft_bzero:
+;; 	;; push rcx
+;; 	mov eax, 0
+;; 	mov rcx, rsi
+;; 	rep stosd
+;; 	;; pop rcx
+;; 	ret
